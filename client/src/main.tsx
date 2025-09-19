fe90a74479025c4c5b8746a94a7bfaf5e15d399c
Trung DT <thanhtrung.it25@gmail.com>
2025-09-19 17:41:12 +0700

    clean up

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,7 +8,6 @@ import * as TanStackQueryProvider from './integrations/tanstack-query/root-provi
 import { routeTree } from './routeTree.gen'
 
 import './styles.css'
-import reportWebVitals from './reportWebVitals.ts'
 
 // Create a new router instance
 
@@ -44,7 +43,3 @@ if (rootElement && !rootElement.innerHTML) {
   )
 }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
